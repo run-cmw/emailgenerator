bb0fb544d93d25e536ecc0298af66f8d4f9c6bda
Ovini Nanayakkara <ovini@medzii.com>
2019-04-01 08:06:04 -0700

    adding more tests

---
+++ Assignment8/src/test/java/edu/neu/ccs/cs5004/Assignment8/Problem1/NameTest.java
@@ -0,0 +1,12 @@
+package edu.neu.ccs.cs5004.Assignment8.Problem1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+public class NameTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+}
