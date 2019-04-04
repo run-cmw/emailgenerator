fc64fb307a99d5fe0c2baf0764569135f139b302
Gina Park <park.jina@husky.neu.edu>
2019-04-03 23:22:43 -0700

    Got rid of the problem folders

--- Assignment8/src/main/java/edu/neu/ccs/cs5004/assignment8/Problem2/NonUniqueNumberException.java
+++
@@ -1,9 +0,0 @@
-package edu.neu.ccs.cs5004.assignment8.problem2;
-
-/**
- * This class creates a new exception and is thrown if a license number is already taken.
- */
-public class NonUniqueNumberException extends Exception {
-  public NonUniqueNumberException(String message) {super(message);}
-
-}
