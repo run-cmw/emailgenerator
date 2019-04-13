859d5af1130c41a35fe00630578cb82cfe87b5ac
Clara Mae Wells <wells.cl@husky.neu.edu>
2019-04-13 12:56:19 -0700

    add most updated test - still not working properly

--- assignment9/src/test/java/edu/neu/css/cs5004/CSVProcessorTest.java
+++ assignment9/src/test/java/edu/neu/css/cs5004/CSVProcessorTest.java
@@ -9,42 +9,79 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CSVProcessorTest {
-  private final String MEMBER_INFO_FILE = "insurance_company_members.csv";
-  private CSVProcessor processor;
-  private List<List<String>> headerArrayList;
-  private List<List<String>> memberInfoArrayList;
-  private List<List<String>> sameHeaderList;
-  private List<List<String>> differentHeaderList;
-  private List<List<String>> sameMemberInfoList;
-  private List<List<String>> differentMemberInfoList;
+  protected CSVProcessor processor;
+  protected List<List<String>> headerArrayList;
+  protected List<List<String>> memberInfoArrayList;
+  protected List<List<String>> sameHeaderList; // move this and following 3 vars to respective equals testers?
+  protected List<List<String>> differentHeaderList;
+  protected List<List<String>> sameMemberInfoList;
+  protected List<List<String>> differentMemberInfoList;
 
   @Before
   public void setUp() throws Exception {
+    final String MEMBER_INFO_FILE = "insurance_company_members.csv";
     processor = new CSVProcessor(MEMBER_INFO_FILE);
+
+    sameHeaderList = new ArrayList<>(); // correctly populate this and following three vars
+    differentHeaderList = new ArrayList<>();
+    sameMemberInfoList = new ArrayList<>();
+    differentMemberInfoList = new ArrayList<>();
   }
 
   @Test
   public void testParseHeader() {
-    final String HEADER_STRING = "[first_name, last_name, company_name, address, city, county, state, zip, phone1, phone2, email, web]";
+    final String HEADER_STRING = "[first_name, last_name, company_name, address, city, county, "
+        + "state, zip, phone1, phone2, email, web]";
     System.out.println(headerArrayList);
 
     assertEquals(HEADER_STRING, headerArrayList.toString());
   }
 
-//  @Test
-//  public void testParseMemberInfo() {
-//    final String MEMBER_INFO_STRING = "James, Butt, Benton, John B Jr, 6649 N Blue Gum St, New Orleans, Orleans, LA, 70116, 504-621-8927, 504-845-1427, jbutt@gmail.com, http://www.bentonjohnbjr.com";
-//    final String MEMBER_COMPANY_STRING = "Benton, John B Jr";
-//
-//    assertEquals(MEMBER_INFO_STRING, memberInfoArrayList.get(0).toString());
-//    assertEquals(MEMBER_COMPANY_STRING, memberInfoArrayList.get(0).get(2));
-//  }
-//
-//  @Test
-//  public void testHeaderEquals() {
-//  }
-//
-//  @Test
-//  public void testInfoEquals() {
-//  }
+  @Test
+  public void testParseMemberInfo() {
+    final String MEMBER_INFO_STRING = "James, Butt, Benton, John B Jr, 6649 N Blue Gum St, New Orleans, Orleans, LA, 70116, 504-621-8927, 504-845-1427, jbutt@gmail.com, http://www.bentonjohnbjr.com";
+    final String MEMBER_COMPANY_STRING = "Benton, John B Jr";
+
+    assertEquals(MEMBER_INFO_STRING, memberInfoArrayList.get(0).toString());
+    assertEquals(MEMBER_COMPANY_STRING, memberInfoArrayList.get(0).get(2));
+  }
+
+  @Test
+  public void testHeaderEquals() {
+    final String DIFFERENT_DATA_TYPE1 = "first_name, last_name, company_name, address, city, county, "
+        + "state, zip, phone1, phone2, email, web";
+    final int DIFFERENT_DATA_TYPE2 = 0;
+    final float DIFFERENT_DATA_TYPE3 = 0.5f;
+    final CSVProcessor DIFFERENT_DATA_TYPE4 = processor;
+
+    assertEquals(headerArrayList, sameHeaderList);
+    assertEquals(headerArrayList, headerArrayList);
+    assertEquals(sameHeaderList, sameHeaderList);
+    assertEquals(differentHeaderList, differentHeaderList);
+    assertNotEquals(headerArrayList, differentHeaderList);
+    assertNotEquals(headerArrayList, DIFFERENT_DATA_TYPE1);
+    assertNotEquals(headerArrayList, DIFFERENT_DATA_TYPE2);
+    assertNotEquals(headerArrayList, DIFFERENT_DATA_TYPE3);
+    assertNotEquals(headerArrayList, DIFFERENT_DATA_TYPE4);
+  }
+
+  @Test
+  public void testInfoEquals() {
+    final String DIFFERENT_DATA_TYPE1 = "James, Butt, Benton, John B Jr, 6649 N Blue Gum St, "
+        + "New Orleans, Orleans, LA, 70116, 504-621-8927, 504-845-1427, "
+        + "jbutt@gmail.com, http://www.bentonjohnbjr.com";
+    final int DIFFERENT_DATA_TYPE2 = 0;
+    final float DIFFERENT_DATA_TYPE3 = 0.5f;
+    final CSVProcessor DIFFERENT_DATA_TYPE4 = processor;
+
+    assertEquals(memberInfoArrayList, sameMemberInfoList);
+    assertEquals(memberInfoArrayList, memberInfoArrayList);
+    assertEquals(sameMemberInfoList, sameMemberInfoList);
+    assertEquals(differentMemberInfoList, differentMemberInfoList);
+    assertNotEquals(memberInfoArrayList, differentMemberInfoList);
+    assertNotEquals(memberInfoArrayList, DIFFERENT_DATA_TYPE1);
+    assertNotEquals(memberInfoArrayList, DIFFERENT_DATA_TYPE2);
+    assertNotEquals(memberInfoArrayList, DIFFERENT_DATA_TYPE3);
+    assertNotEquals(memberInfoArrayList, DIFFERENT_DATA_TYPE4);
+  }
 }
