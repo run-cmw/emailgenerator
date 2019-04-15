22b69fa559db9554d7b727014080bb4d6addaa86
Ovini Nanayakkara <ovini@medzii.com>
2019-04-14 17:53:48 -0700

    GenerateMail.java work in progress

--- assignment9/src/main/java/edu/neu/css/cs5004/GenerateMail.java
+++ assignment9/src/main/java/edu/neu/css/cs5004/GenerateMail.java
@@ -10,18 +10,20 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-public class GenerateMail{
-  private final String MEMBER_INFO_FILE = "insurance_company_members.csv";
+public class GenerateMail {
 
-  private List<List<String>> headers;
-  private List<String> result;
-  private List<List<String>> currentCSVline;
-  private List<String> curCSVresult;
+  private final String MEMBER_INFO_FILE = "insurance_company_members_copy.csv";
+
+  private List<List<String>> rawHeaders;
+  private List<String> headers;
+  private List<List<String>> members;
   private ReadTemplate newReadTemplate = new ReadTemplate();
   private CSVProcessor newCSVProcessor = new CSVProcessor(MEMBER_INFO_FILE);
 
@@ -30,117 +32,47 @@ public class GenerateMail{
   }
 
 
-  private List<String> convert(List<List<String>> headers){
-    for (List<String> list : headers) {
-      result.addAll(list);
+  public void generateMail(String templateName, String fileName, MailType mailType,
+      String outputDir) {
 
-    }
-    return result;
-
-  }
-
-
-
-  public void generateMail(String templateName, String fileName, String mailType, String outputDir) throws Exception{
-
-    try {
-
-      headers = newCSVProcessor.parseHeader();
-      result = convert(headers);
+    rawHeaders = newCSVProcessor.getHeaderArrayList();
+    System.out.println("printing raw header");
+    System.out.println(rawHeaders);
+    headers = Arrays.asList(rawHeaders.get(0).get(0).split(","));
+    System.out.println("ptingting headers");
+    System.out.println(headers);
 
 
-      System.out.println(System.getProperty("user.dir"));
+    members = newCSVProcessor.getMemberInfoArrayList();
+    //System.out.println("printing members");
+    //System.out.println(members);
 
-      BufferedReader inputFile = null;
-      inputFile = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+    String template = newReadTemplate.parseTemplate(templateName);
 
-      String eachCSVline;
-      inputFile.readLine();
-      System.out.println("starting ....");
-      while((eachCSVline = inputFile.readLine()) != null){
-        System.out.println("Read: " + eachCSVline);
-        currentCSVline = newCSVProcessor.parseHeader();
-        curCSVresult = convert(currentCSVline);
+    // System.out.println(System.getProperty("user.dir"));
 
+    for (List<String> member : members) {
 
-        StringBuilder newGroup = new StringBuilder();
-        Pattern re;
+      String memberMail = template;
+      System.out.println("printing member : " + member);
 
-        String template = newReadTemplate.parseTemplate(templateName);
-
-        for (int i = 0; i < result.size(); i++) {
-          newGroup.append("\\[\\[").append(result.get(i).replace("\"", "")).append("\\]\\]");
-          re = Pattern.compile(newGroup.toString());
-          Matcher newMatcher = re.matcher(template);
-          if (newMatcher.find()) {
-            template = template.replace(newMatcher.group(),
-                curCSVresult.get(i).replace("\"", ""));
-          }
-          newGroup.setLength(0);
-        }
-
-        String outputFileName = mailType;
-        outputFileName = outputFileName + "_"
-            + curCSVresult.get(0).replace("\"", "") + " "
-            + curCSVresult.get(1).replace("\"", "") + ".txt";
-        BufferedWriter outputFile = new BufferedWriter(
-            new OutputStreamWriter(
-                new FileOutputStream(outputDir + File.separator + outputFileName)));
-        outputFile.write(template);
-        outputFile.flush();
-        outputFile.close();
-
+      for (int i = 0; i < headers.size(); i++) {
+        String currentHeader = headers.get(i);
+        System.out.println("printing current header");
+        System.out.println(currentHeader);
+        String memberProperty = member.get(i);
+        //System.out.println(memberProperty);
+        String stringToReplace = "[[" + currentHeader + "]]";
+        memberMail = memberMail.replaceAll(stringToReplace, memberProperty);
 
       }
+      System.out.println( "printing member mail: ");
+      System.out.println(memberMail);
 
-      inputFile.close();
-
-    } catch (FileNotFoundException fnfe) {
-      System.out.println("*** OOPS! A file was not found : " + fnfe.getMessage());
-      fnfe.printStackTrace();
-    } catch (IOException ioe) {
-      System.out.println("Something went wrong! : " + ioe.getMessage());
-      ioe.printStackTrace();
     }
 
   }
 
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) {
-      return true;
-    }
-    if (!(obj instanceof GenerateMail)) {
-      return false;
-    }
-    GenerateMail that = (GenerateMail) obj;
-    return MEMBER_INFO_FILE.equals(that.MEMBER_INFO_FILE) &&
-        headers.equals(that.headers) &&
-        result.equals(that.result) &&
-        currentCSVline.equals(that.currentCSVline) &&
-        curCSVresult.equals(that.curCSVresult) &&
-        newReadTemplate.equals(that.newReadTemplate) &&
-        newCSVProcessor.equals(that.newCSVProcessor);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects
-        .hash(MEMBER_INFO_FILE, headers, result, currentCSVline, curCSVresult, newReadTemplate,
-            newCSVProcessor);
-  }
-
-
-  @Override
-  public String toString() {
-    return "GenerateMail{" +
-        "MEMBER_INFO_FILE='" + MEMBER_INFO_FILE + '\'' +
-        ", headers=" + headers +
-        ", result=" + result +
-        ", currentCSVline=" + currentCSVline +
-        ", curCSVresult=" + curCSVresult +
-        ", newReadTemplate=" + newReadTemplate +
-        ", newCSVProcessor=" + newCSVProcessor +
-        '}';
-  }
 }
+
+
