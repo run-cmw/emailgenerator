5505476927c226719e09005fa02f49b3931e66c6
Ovini Nanayakkara <ovini@medzii.com>
2019-04-15 18:30:49 -0700

    Adding Javadoc and the email & template folders

--- assignment9/src/main/java/edu/neu/css/cs5004/GenerateMail.java
+++ assignment9/src/main/java/edu/neu/css/cs5004/GenerateMail.java
@@ -7,25 +7,55 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * This class represents Generating a mail functionality
+ */
 
 public class GenerateMail {
 
   private final String MEMBER_INFO_FILE = "insurance_company_members_copy.csv";
 
-
+  /**
+   * List of headers
+   */
   private List<String> headers;
+  /**
+   * List of List containing string of members
+   */
   private List<List<String>> members;
+  /**
+   * Create a new object of Readtemplate
+   */
   private ReadTemplate newReadTemplate = new ReadTemplate();
+
+  /**
+   * Create a new object of CSVProcsessor
+   */
   private CSVProcessor newCSVProcessor = new CSVProcessor(MEMBER_INFO_FILE);
+
+  /**
+   * newFileName as a String
+   */
   private String newFileName;
 
-
+  /**
+   * Constructor to GenerateMail
+   * @throws IOException Exception thrown
+   */
   public GenerateMail() throws IOException {
 
   }
 
-
+  /**
+   * Method to generate Mail
+   * @param templateName templateName
+   * @param fileName fileName
+   * @param mailType mailType
+   * @param outputDir outputDir
+   * @throws Exception
+   */
   public void generateMail(String templateName, String fileName, MailType mailType, String outputDir) throws Exception{
 
     headers = newCSVProcessor.getHeaderArrayList();
@@ -45,17 +75,12 @@ public class GenerateMail {
 
       }
 
-      //System.out.println( "printing member mail: ");
-      //System.out.println(memberMail);
-
       Enum OutPutFileName = mailType;
 
       //System.out.println("OutputFile name is" + OutPutFileName);
       newFileName = OutPutFileName + "_"
           + member.get(0).replace("\"", "") + "_"
           + member.get(1).replace("\"", "") + ".txt";
-
-      //System.out.println(newFileName);
 
       BufferedWriter outputFile = new BufferedWriter( new OutputStreamWriter(new FileOutputStream(outputDir + File.separator + newFileName)));
       outputFile.write(memberMail);
@@ -67,7 +92,56 @@ public class GenerateMail {
 
   }
 
+  /**
+   * Indicates whether some other object is "equal to" this one.
+   *
+   * @param obj - the reference object with which to compare
+   * @return {@code true} if this object is the same as the obj argument and {@code false} otherwise
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (!(obj instanceof GenerateMail)) {
+      return false;
+    }
+    GenerateMail that = (GenerateMail) obj;
+    return MEMBER_INFO_FILE.equals(that.MEMBER_INFO_FILE) &&
+        headers.equals(that.headers) &&
+        members.equals(that.members) &&
+        newReadTemplate.equals(that.newReadTemplate) &&
+        newCSVProcessor.equals(that.newCSVProcessor) &&
+        newFileName.equals(that.newFileName);
+  }
 
+  /**
+   * Returns a hash code value for the object.
+   *
+   * @return hash code value for the object
+   */
+  @Override
+  public int hashCode() {
+    return Objects
+        .hash(MEMBER_INFO_FILE, headers, members, newReadTemplate, newCSVProcessor, newFileName);
+  }
+
+  /**
+   * Returns a string representation of the object
+   * @return String representation of the object
+   */
+
+  @Override
+  public String toString() {
+    return "GenerateMail{" +
+        "MEMBER_INFO_FILE='" + MEMBER_INFO_FILE + '\'' +
+        ", headers=" + headers +
+        ", members=" + members +
+        ", newReadTemplate=" + newReadTemplate +
+        ", newCSVProcessor=" + newCSVProcessor +
+        ", newFileName='" + newFileName + '\'' +
+        '}';
+  }
 }
 
 
