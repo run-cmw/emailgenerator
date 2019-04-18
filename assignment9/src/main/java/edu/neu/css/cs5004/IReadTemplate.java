64568cd726d6450f8299679486deb7aaf5568f63
Ovini Nanayakkara <ovini@medzii.com>
2019-04-17 19:02:49 -0700

    Adding the Igenerate and IReadtemplate files

---
+++ assignment9/src/main/java/edu/neu/css/cs5004/IReadTemplate.java
@@ -0,0 +1,18 @@
+package edu.neu.css.cs5004;
+
+/**
+ * This interface represents the IReadTemplate.
+ */
+public interface IReadTemplate {
+
+  /**
+   * Method to parse the template.
+   * @param templateName of template to be parsed
+   * @return String representation of the template
+   */
+
+  String parseTemplate(String templateName);
+
+
+
+}
